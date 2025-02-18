d8a82ebcc4b3057e266288bd0f9eb10b5ae5d148
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-02-18 18:38:55 +0800

    finished

--- Lectures/Test_code/Getting_Start.py
+++ Lectures/Test_code/Getting_Start.py
@@ -17,18 +17,4 @@ name = input("Please enter your name: ")
 print(f"Hello, {name}!")
 
 ##########
-
-def hailstone_conjecture(n):
-    sequence = [n]
-    while n != 1:
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = 3 * n + 1
-        sequence.append(n)
-    return sequence
-
-
-start_number = input("Please enter a number: ")
-result = hailstone_conjecture(start_number)
-print(f"The hailstone sequence starting from {start_number} is: {result}")
+###end
